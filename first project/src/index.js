42db657168529b84e7c97aa1503a2963f7a7d0e2
PhilipSh <fil3120025@gmail.com>
2018-07-17 15:13:04 +0300

    show result of search

--- first project/src/index.js
+++ first project/src/index.js
@@ -1,4 +1,5 @@
 import './less/main.less';
 import './less/article.less';
+import './less/searchResult.less';
 import './js/search';
 import './js/render';
--- first project/src/js/search.js
+++ first project/src/js/search.js
@@ -1,6 +1,20 @@
-function render(response){
-    let li = document.getElementById('li');
-    li.innerHTML = response.data.recipes[0].title;
+function renderSearch(response) {
+    const result = document.getElementsByClassName('resultSearch')[0];
+    while (result.firstChild) {
+        result.removeChild(result.firstChild);
+    }
+    for (let i = 0; i < 30; i++) {
+        let imgDish = document.createElement('img');
+        let new_li = document.createElement('li');
+        let title = document.createElement('p');
+        title.innerHTML = response.data.recipes[i].title;
+        imgDish.src = response.data.recipes[i].image_url;
+        result.appendChild(new_li);
+        new_li.appendChild(title);
+        new_li.appendChild(imgDish);
+        new_li.className = 'recipeDish';
+        getIngredients(response.data.recipes[i].recipe_id, new_li);
+    }
 }
 
 window.onload = function getRecipes() {
@@ -14,10 +28,32 @@ window.onload = function getRecipes() {
     })
         .then(function (response) {
             console.log(response)
-            render(response);
+            renderSearch(response);
         })
         .catch(function (error) {
             console.log(error);
         })
-        but.addEventListener('click', getRecipes, false);
+    but.addEventListener('click', getRecipes, false);
+}
+function getIngredients(recipe_id, new_li) {
+    axios.get('https://cors.io/?http://food2fork.com/api/get?', {
+        params: {
+            key: '14d4f957bf943fe0ae9172647a0f4d1f',
+            rId: recipe_id
+        }
+    })
+        .then(function (response) {
+            console.log(response);
+            renderIngredients(response, new_li);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+function renderIngredients(response, new_li) {
+    let p = document.createElement('p');
+    for (let lengthIngredients = 0; lengthIngredients < response.data.recipe.ingredients.length; lengthIngredients++) {
+        p.innerHTML = p.innerHTML + response.data.recipe.ingredients[lengthIngredients] + ', ';
+    }
+    new_li.appendChild(p);
 }
